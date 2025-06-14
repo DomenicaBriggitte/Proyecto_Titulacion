0a4296e1d089f63f03836830475b402110775fac
Manuel Burgos <mburgosramirez@gmail.com>
2025-06-13 22:06:26 -0500

    merge resuelto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,26 +13,13 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatBadgeModule } from '@angular/material/badge';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    
     ClienteComponent,
     PedidoComponent,
     FacturaComponent,
@@ -48,17 +35,6 @@ import { MatBadgeModule } from '@angular/material/badge';
     AppRoutingModule,
     CommonModule,
     FormsModule,
-    MatTableModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatInputModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatCardModule,
-    BrowserAnimationsModule,
-    MatChipsModule,
-    MatBadgeModule
     AppRoutingModule
   ],
   providers: [],
