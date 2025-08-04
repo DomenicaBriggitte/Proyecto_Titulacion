8674ff95f9fe1437a38297cecd87c3736e06b95a
mburgosr <mburgosramirez@gmail.com>
2025-08-04 16:10:16 -0500

    Correción creación de cliente y ordenados alfabéticamente

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -6,341 +6,332 @@ import * as XLSX from 'xlsx';
 import * as FileSaver from 'file-saver';
 
 @Component({
-  selector: 'app-cliente',
-  templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.css']
+ selector: 'app-cliente',
+ templateUrl: './cliente.component.html',
+ styleUrls: ['./cliente.component.css']
 })
 export class ClienteComponent {
-clientes: Cliente[] = [];
-filteredClientes: any[] = [];
-filtroTipo: string = '';
-searchQuery: string = '';
-currentPage: number = 1;
+ clientes: Cliente[] = [];
+ filteredClientes: any[] = [];
+ filtroTipo: string = '';
+ searchQuery: string = '';
+ currentPage: number = 1;
  mensajeError: string = ''; 
-errorClienteExistente: boolean = false; // Variable para activar el mensaje de error
+ errorClienteExistente: boolean = false; // Variable para activar el mensaje de error
 
-ngOnInit() {
+ ngOnInit() {
+  this.cargarClientes();
+ }
+
+ constructor(private clienteService: ClienteService) {}
+
+ // Nuevo método para cargar clientes, que se puede reutilizar
+ cargarClientes(): void {
   this.clienteService.getClientes().subscribe(data => {
-    this.clientes = data;
-    this.filteredClientes = [...this.clientes]; 
-    this.currentPage = 1// Esto es importante para paginar
+   this.clientes = data;
+   this.filteredClientes = [...this.clientes];
+   this.currentPage = 1; // Esto es importante para paginar
   });
-}
+ }
 
-constructor(private clienteService: ClienteService) {}
-
-  abs(value: number): number {
+ abs(value: number): number {
   return Math.abs(value);
-}
-  
+ }
+ 
+ nuevoCliente = { cedula: '', nombre: '', tipo: '', telefono: '', correo: '' };
+ selectedCliente = { cedula: '', nombre: '', tipo: '', telefono: '', correo: '' };
 
-  nuevoCliente = { cedula: '', nombre: '', tipo: '', telefono: '', correo: '' };
-  selectedCliente = { cedula: '', nombre: '', tipo: '', telefono: '', correo: '' };
 
-
-filterClientes() {
+ filterClientes() {
   const tipo = this.filtroTipo;
   this.filteredClientes = this.clientes.filter(cliente => {
-    const coincideTipo = tipo ? cliente.tipo === tipo : true;
-    const coincideBusqueda = 
-    cliente.cedula.includes(this.searchQuery) || 
-    cliente.nombre.toLowerCase().includes(this.searchQuery.toLowerCase());
+   const coincideTipo = tipo ? cliente.tipo === tipo : true;
+   const coincideBusqueda = 
+   cliente.cedula.includes(this.searchQuery) || 
+   cliente.nombre.toLowerCase().includes(this.searchQuery.toLowerCase());
 
-    return coincideTipo && coincideBusqueda;
-});
+   return coincideTipo && coincideBusqueda;
+  });
   this.currentPage = 1; // importante para evitar mostrar más registros de los que debe
-}
+ }
 
-  // Paginación
-itemsPerPage: number = 10;
+ // Paginación
+ itemsPerPage: number = 10;
 
-get totalPages(): number {
+ get totalPages(): number {
   return Math.ceil(this.filteredClientes.length / this.itemsPerPage);
-}
+ }
 
-get paginatedClientes(): any[] {
+ get paginatedClientes(): any[] {
   const start = (this.currentPage - 1) * this.itemsPerPage;
   const end = start + this.itemsPerPage;
   return this.filteredClientes.slice(start, end);
-}
+ }
 
-// Para mostrar solo 3 botones de página
-get paginationRange(): number[] {
+ // Para mostrar solo 3 botones de página
+ get paginationRange(): number[] {
   const total = this.totalPages;
   let start = Math.max(1, this.currentPage - 1);
   let end = Math.min(total, start + 1); 
 
   if (end - start < 1) {
-    start = Math.max(1, end - 1);
+   start = Math.max(1, end - 1);
   }
 
   const range: number[] = [];
   for (let i = start; i <= end; i++) {
-    range.push(i);
+   range.push(i);
   }
   return range;
-}
+ }
 
-// Función para cambiar de página
-onItemsPerPageChange(): void {
+ // Función para cambiar de página
+ onItemsPerPageChange(): void {
   this.currentPage = 1;
-}
+ }
 
-exportarExcel() {
+ exportarExcel() {
   const worksheet = XLSX.utils.json_to_sheet(this.filteredClientes);
   const workbook = {
-    Sheets: { 'Clientes': worksheet },
-    SheetNames: ['Clientes']
+   Sheets: { 'Clientes': worksheet },
+   SheetNames: ['Clientes']
   };
   const excelBuffer: any = XLSX.write(workbook, {
-    bookType: 'xlsx',
-    type: 'array'
+   bookType: 'xlsx',
+   type: 'array'
   });
 
   const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
   FileSaver.saveAs(blob, 'Clientes.xlsx');
-}
-  // Validación de la cédula para asegurarse de que solo contiene números
-  validateCedula(event: KeyboardEvent) {
-    const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
-    const regex = /^[0-9]*$/; // Solo números
+ }
+ // Validación de la cédula para asegurarse de que solo contiene números
+ validateCedula(event: KeyboardEvent) {
+  const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
+  const regex = /^[0-9]*$/; // Solo números
 
-    // Permitir teclas de control como Backspace, Tab, y las flechas
-    if (allowedKeys.indexOf(event.key) !== -1) {
-      return; // Permite teclas de control
-    }
-
-    // Si la tecla presionada no es un número, evitar que el valor se agregue
-    if (!regex.test(event.key)) {
-      event.preventDefault(); // Bloquear la tecla no permitida
-    }
+  // Permitir teclas de control como Backspace, Tab, y las flechas
+  if (allowedKeys.indexOf(event.key) !== -1) {
+   return; // Permite teclas de control
   }
 
-    // Validación del nombre para asegurarse de que solo contenga letras y puntos
-    validateName(event: KeyboardEvent) {
-    const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
-    const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]*$/; // Solo letras, espacios y puntos
+  // Si la tecla presionada no es un número, evitar que el valor se agregue
+  if (!regex.test(event.key)) {
+   event.preventDefault(); // Bloquear la tecla no permitida
+  }
+ }
 
-    // Permitir teclas de control como Backspace, Tab, y las flechas
-    if (allowedKeys.indexOf(event.key) !== -1) {
-      return; // Permite teclas de control
-    }
+ // Validación del nombre para asegurarse de que solo contenga letras y puntos
+ validateName(event: KeyboardEvent) {
+  const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
+  const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ. ]*$/; // Solo letras, espacios y puntos
 
-    // Si la tecla presionada no es un carácter permitido, evitar que el valor se agregue
-    if (!regex.test(event.key)) {
-      event.preventDefault(); // Bloquear la tecla no permitida
-    }
+  // Permitir teclas de control como Backspace, Tab, y las flechas
+  if (allowedKeys.indexOf(event.key) !== -1) {
+   return; // Permite teclas de control
   }
 
-  //Validación teléfono
-  validatePhone(event: KeyboardEvent) {
-    const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
-    const regex = /^[0-9]*$/; // Solo números
-
-    // Permitir teclas de control como Backspace, Tab, y las flechas
-    if (allowedKeys.indexOf(event.key) !== -1) {
-      return; // Permite teclas de control
-    }
-    if (!regex.test(event.key)) {
-      event.preventDefault(); // Bloquear la tecla no permitida
-    }
+  // Si la tecla presionada no es un carácter permitido, evitar que el valor se agregue
+  if (!regex.test(event.key)) {
+   event.preventDefault(); // Bloquear la tecla no permitida
   }
+ }
 
-  
-validateEmail(event: KeyboardEvent) {
+ //Validación teléfono
+ validatePhone(event: KeyboardEvent) {
+  const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
+  const regex = /^[0-9]*$/; // Solo números
+
+  // Permitir teclas de control como Backspace, Tab, y las flechas
+  if (allowedKeys.indexOf(event.key) !== -1) {
+   return; // Permite teclas de control
+  }
+  if (!regex.test(event.key)) {
+   event.preventDefault(); // Bloquear la tecla no permitida
+  }
+ }
+
+ validateEmail(event: KeyboardEvent) {
   const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'Delete'];
   const regex = /^[a-zA-Z0-9._%+-@.]*$/; // Solo caracteres válidos
 
   // Bloquear espacio
   if (event.key === ' ') {
-    event.preventDefault();
-    return;
+   event.preventDefault();
+   return;
   }
 
   if (allowedKeys.includes(event.key)) {
-    return; // Permitir teclas de control
+   return; // Permitir teclas de control
   }
 
   if (!regex.test(event.key)) {
-    event.preventDefault(); // Bloquear teclas inválidas
+   event.preventDefault(); // Bloquear teclas inválidas
   }
-}
+ }
 
+ // Agregar nuevo cliente
 // Agregar nuevo cliente
 addClient(form: NgForm) {
   if (form.invalid) return;
 
   this.clienteService.addCliente(this.nuevoCliente).subscribe({
     next: () => {
-      // Vuelve a cargar la lista desde la API
-      this.clienteService.getClientes().subscribe((clientes: any[]) => {
-        this.clientes.unshift(this.nuevoCliente); 
-        this.filteredClientes = [...this.clientes];
-        this.currentPage = 1; // Asegurarse de que la paginación empiece desde la primera página
-      });
-
+      this.cargarClientes(); 
       this.nuevoCliente = { cedula: '', nombre: '', tipo: '', telefono: '', correo: '' };
 
-      // Cierra el modal y muestra éxito
       const nuevoClienteModalElement = document.getElementById('nuevoClienteModal');
       if (nuevoClienteModalElement) {
-        const modal = bootstrap.Modal.getInstance(nuevoClienteModalElement) || new bootstrap.Modal(nuevoClienteModalElement);
-        modal.hide();
-      }
-      const successModalElement = document.getElementById('successModal');
-      if (successModalElement) {
-        const successModal = new bootstrap.Modal(successModalElement);
-        successModal.show();
-        setTimeout(() => {
-          const backdrop = document.querySelector('.modal-backdrop');
-          if (backdrop) backdrop.remove();
-        }, 300);
+        const modal = bootstrap.Modal.getInstance(nuevoClienteModalElement);
+        modal?.hide();
+
+        // Cuando se cierre el modal, mostramos el de éxito
+        nuevoClienteModalElement.addEventListener('hidden.bs.modal', () => {
+          const successModalElement = document.getElementById('successModal');
+          if (successModalElement) {
+            const successModal = new bootstrap.Modal(successModalElement);
+            successModal.show();
+
+            // Cierra automáticamente después de 1.5 seg
+            setTimeout(() => {
+              successModal.hide();
+
+              // 🔹 Limpiar cualquier backdrop residual
+              document.querySelectorAll('.modal-backdrop').forEach(el => el.remove());
+            }, 1500);
+          }
+        }, { once: true }); // Solo ejecutar una vez
       }
     },
     error: (err) => {
       if (err.status === 409) {
-        // Si el error es un 409, significa que el cliente ya está registrado
         this.errorClienteExistente = true;
         this.mensajeError = err.error.message || "Este cliente ya está registrado.";
       } else {
-        // Para otros errores
         alert('Error al agregar cliente: ' + (err.error?.message || err.message));
       }
     }
   });
 }
 
-  
-
-// Función para editar un cliente
-editCliente(cliente: any) {
-  // Copiar los datos del cliente seleccionado en selectedCliente
+ // Función para editar un cliente
+ editCliente(cliente: any) {
   this.selectedCliente = { ...cliente }; 
-  // Abrir el modal de edición
   const modalElement = document.getElementById('editClientModal');
   if (modalElement) {
-    const modal = new bootstrap.Modal(modalElement);
-    modal.show(); // Mostrar el modal
+   const modal = new bootstrap.Modal(modalElement);
+   modal.show();
+   modal.hide();
   }
-}
+ }
 
-
-// Actualizar cliente
-updateCliente() {
+ // Actualizar cliente
+ updateCliente() {
   this.clienteService.updateCliente(this.selectedCliente).subscribe({
-    next: () => {
-      const index = this.clientes.findIndex(c => c.cedula === this.selectedCliente.cedula);
-      if (index !== -1) {
-        this.clientes[index] = { ...this.selectedCliente };
-        this.filteredClientes = [...this.clientes];
-        this.currentPage = 1
+   next: () => {
+    const index = this.clientes.findIndex(c => c.cedula === this.selectedCliente.cedula);
+    if (index !== -1) {
+     this.clientes[index] = { ...this.selectedCliente };
+     this.filteredClientes = [...this.clientes];
+     this.currentPage = 1
+    }
+  
+    // Cierra el modal de edición y espera a que se oculte por completo
+    const editClientModalElement = document.getElementById('editClientModal');
+    if (editClientModalElement) {
+     const modal = bootstrap.Modal.getInstance(editClientModalElement);
+     modal?.hide();
+
+     // Después de que el modal se oculte, muestra el modal de éxito
+     editClientModalElement.addEventListener('hidden.bs.modal', () => {
+      const successModalElement = document.getElementById('successModal');
+      if (successModalElement) {
+       const successModal = new bootstrap.Modal(successModalElement);
+       successModal.show();
+       // Cierra el modal de éxito automáticamente después de 2 segundos
+       setTimeout(() => {
+        successModal.hide();
+       }, 1000);
       }
- 
-      // Cierra el modal de edición después de guardar
-      const editClientModalElement = document.getElementById('editClientModal');
-      if (editClientModalElement) {
-        const modal = bootstrap.Modal.getInstance(editClientModalElement);
-        modal?.hide(); // Cierra el modal de edición
-      }
-
-
-    // Mostrar el modal de éxito
-    const successModalElement = document.getElementById('successModal');
-    if (successModalElement) {
-      const successModal = new bootstrap.Modal(successModalElement);
-      successModal.show(); // Mostrar el modal de éxito
-      successModal.hide();
+     }, { once: true }); // Usar { once: true } para que el evento se elimine automáticamente
     }
-
-    // Asegurarse de que el backdrop se elimine al cerrar el modal
-    const backdrop = document.querySelector('.modal-backdrop');
-    if (backdrop) {
-      backdrop.remove();
-    }
-  },
-    error: (err) => {
-      alert('Error al actualizar cliente: ' + (err.error?.message || err.message));
-    }
+   },
+   error: (err) => {
+    alert('Error al actualizar cliente: ' + (err.error?.message || err.message));
+   }
   });
-}
+ }
 
-get clientesNaturales(): Cliente[] {
+ get clientesNaturales(): Cliente[] {
   return this.clientes.filter(c => c.tipo === 'Natural');
-}
+ }
 
-get clientesJuridicos(): Cliente[] {
+ get clientesJuridicos(): Cliente[] {
   return this.clientes.filter(c => c.tipo === 'Jurídica');
-}
+ }
 
-filtrarPorTipo(tipo: string): void{
+ filtrarPorTipo(tipo: string): void{
   this.filtroTipo = tipo;
   this.filterClientes();
   this.currentPage = 1;
-}
+ }
 
-clienteParaEliminar: any = null;
-deleteCliente(cliente: any) {
+ clienteParaEliminar: any = null;
+ deleteCliente(cliente: any) {
   this.clienteParaEliminar = cliente;
   const confirmModalElement = document.getElementById('confirmDeleteModal');
   if (confirmModalElement) {
-    const confirmModal = new bootstrap.Modal(confirmModalElement);
-    confirmModal.show();
-    confirmModal.hide();
+   const confirmModal = new bootstrap.Modal(confirmModalElement);
+   confirmModal.show();
   }
-      // Asegurarse de que el backdrop se elimine al cerrar el modal
-    const backdrop = document.querySelector('.modal-backdrop');
-    if (backdrop) {
-      backdrop.remove(); // Eliminar la capa oscura
-    }
-}
+ }
 
-// Eliminar cliente
-confirmDelete() {
+ // Eliminar cliente
+ confirmDelete() {
   this.clienteService.deleteCliente(this.clienteParaEliminar.cedula).subscribe({
-    next: () => {
-      const index = this.clientes.findIndex(c => c.cedula === this.clienteParaEliminar.cedula);
-      if (index !== -1) {
-        this.clientes.splice(index, 1);
-        this.filteredClientes = [...this.clientes];
-        this.currentPage = 1
-      }
-      this.clienteParaEliminar = null;
-      // Cierra el modal de confirmación
-        const confirmModalElement = document.getElementById('confirmDeleteModal');
-          if (confirmModalElement) {
-            const modal = bootstrap.Modal.getInstance(confirmModalElement);
-            modal?.hide();
-          }
-      
-        // Muestra el modal de éxito
-        const deleteModalElement = document.getElementById('deleteModal');
-        if (deleteModalElement) {
-          const deleteModal = new bootstrap.Modal(deleteModalElement);
-          deleteModal.show();
-        }
-      
-        // Limpia la variable
-        this.clienteParaEliminar = null;
-        
-    },
-    error: (err) => {
-      alert('Error al eliminar cliente: ' + (err.error?.message || err.message));
+   next: () => {
+    const index = this.clientes.findIndex(c => c.cedula === this.clienteParaEliminar.cedula);
+    if (index !== -1) {
+     this.clientes.splice(index, 1);
+     this.filteredClientes = [...this.clientes];
+     this.currentPage = 1
     }
+    
+    // Cierra el modal de confirmación y espera a que se oculte por completo
+    const confirmModalElement = document.getElementById('confirmDeleteModal');
+    if (confirmModalElement) {
+     const modal = bootstrap.Modal.getInstance(confirmModalElement);
+     modal?.hide();
+
+     // Después de que el modal se oculte, muestra el modal de eliminación exitosa
+     confirmModalElement.addEventListener('hidden.bs.modal', () => {
+      const deleteModalElement = document.getElementById('deleteModal');
+      if (deleteModalElement) {
+       const deleteModal = new bootstrap.Modal(deleteModalElement);
+       deleteModal.show();
+       // Cierra el modal de éxito automáticamente después de 2 segundos
+       setTimeout(() => {
+        deleteModal.hide();
+       }, 2000);
+      }
+     }, { once: true });
+    }
+    
+    // Limpia la variable
+    this.clienteParaEliminar = null;
+    
+   },
+   error: (err) => {
+    alert('Error al eliminar cliente: ' + (err.error?.message || err.message));
+   }
   });
-}
+ }
 
-  // Función para cerrar un modal y eliminar el backdrop
-  closeModalAndRemoveBackdrop(modalId: string) {
-    const modalElement = document.getElementById(modalId);
-    if (modalElement) {
-      const modal = new bootstrap.Modal(modalElement);
-      modal.hide();  // Cerrar el modal
-
-      // Eliminar el backdrop
-      const backdrop = document.querySelector('.modal-backdrop');
-      if (backdrop) {
-        backdrop.remove();  // Eliminar la capa oscura
-      }
-    }
+ // Función para cerrar un modal
+ closeModalAndRemoveBackdrop(modalId: string) {
+  const modalElement = document.getElementById(modalId);
+  if (modalElement) {
+   const modal = bootstrap.Modal.getInstance(modalElement);
+   modal?.hide();
   }
+ }
 }
